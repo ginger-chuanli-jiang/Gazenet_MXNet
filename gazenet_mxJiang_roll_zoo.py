0806037db4a092886046bb295ce373a9e255054b
ginger-chuanli-jiang <54552591+ginger-chuanli-jiang@users.noreply.github.com>
2021-11-11 09:55:35 -0800

    Update gazenet_mxJiang_roll_zoo.py

--- gazenet_mxJiang_roll_zoo.py
+++ gazenet_mxJiang_roll_zoo.py
@@ -1,16 +1,16 @@
 import mxnet as mx
+import math
+import time
+
 from gluoncv import model_zoo, data, utils
 from matplotlib import pyplot as plt
 from mxnet.gluon import HybridBlock
 from mxnet.gluon.model_zoo import vision
-
 from mxnet import gluon
 from mxnet.gluon import nn
 from mxnet.autograd import record
 from mxnet.gluon import Block
-import math
 from mxnet import nd
-import time
 
 def freeze_bn(block):
     try:
